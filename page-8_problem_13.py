37f455156ddb4566adb9ef27af9bd6905ade3c37
Kkartik14 <85060731+Kkartik14@users.noreply.github.com>
2023-03-02 13:31:39 +0530

    Delete page-8_problem_13.py

--- page-8_problem_13.py
+++
@@ -1,9 +0,0 @@
-# Using membership operator find whether a given character is in a string.
-
-str = input("Enter a string: ")
-char = input("Enter a character: ")
-
-if char in str:
-    print(char, "is in the string.")
-else:
-    print(char, "is not in the string.")
