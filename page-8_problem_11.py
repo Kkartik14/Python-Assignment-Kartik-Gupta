14b1d3249535dbe79719fd89481a69971bb8953a
Kkartik14 <85060731+Kkartik14@users.noreply.github.com>
2023-03-02 13:31:48 +0530

    Delete page-8_problem_11.py

--- page-8_problem_11.py
+++
@@ -1,6 +0,0 @@
-# Write a program to find left shift and right shift values of a given number.
-
-x=int(input("Enter the number: "))
-y=x>>2
-z=x<<2
-print("The number after right shift 2 is {0} and the number after left shift 2 is {1} ".format(y,z))
