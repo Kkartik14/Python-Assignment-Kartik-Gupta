701f9473d5d3fad7d8835895300886ffd61bd4fd
Kkartik14 <85060731+Kkartik14@users.noreply.github.com>
2023-03-02 13:34:08 +0530

    Delete page-5_problem_1.py

--- page-5_problem_1.py
+++
@@ -1,8 +0,0 @@
-n = int(input("enter the value of n: "))
-for i in range(1, n+1):
-    print(" " * (n-i), end="")
-    for j in range(1, i+1):
-        print(j, end=" ")
-    for k in range(i-1, 0, -1):
-        print(k, end=" ")
-    print()
