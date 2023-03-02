e4b02af827a71e996422a3888452007799514531
Kkartik14 <85060731+Kkartik14@users.noreply.github.com>
2023-03-02 13:31:31 +0530

    Delete page-8_problem_5.py

--- page-8_problem_5.py
+++
@@ -1,8 +0,0 @@
-# Write a program to find simple interest.
-
-amt=int(input("Enter Number Amount: "))
-rate=float(input("Enter Number Rate: "))
-time=float(input("Enter Number Time:"))
-
-interest=float((amt*rate*time)/100)
-print("The simple intrest calculated upon the given amount {0}, rate charged {1} and time {2} is : {3}".format(amt,rate,time,interest))
