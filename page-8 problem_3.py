461b4f4aa7e47fc0cfe764ec9f757e70ca90bfb1
Kkartik14 <85060731+Kkartik14@users.noreply.github.com>
2023-03-02 13:33:19 +0530

    Delete page-8 problem_3.py

--- page-8 problem_3.py
+++
@@ -1,6 +0,0 @@
-# Write a Python program to solve (x+y)*(x+y)         Test data : x = 4 , y = 3,          Expected output: 49
-
-x=int(input("Enter value of x: "))
-y=int(input("enter value of y: "))
-output=(x+y)*(x+y)
-print("The output is : {0}".format(output))
