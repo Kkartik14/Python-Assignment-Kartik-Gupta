f3dfe617be21ba6e276faca1d91bcc0b9fbe2a37
Kkartik14 <85060731+Kkartik14@users.noreply.github.com>
2023-03-17 14:56:14 +0530

    Delete page-7_problem_1.py

--- page-7_problem_1.py
+++
@@ -1,6 +0,0 @@
-#Take different data types and print values using print function.
-
-int=int(input("enter your integer:"))
-flt=float(input("enter your decimal number:"))
-str=input("enter your string:")
-print("your integer number is {0} , your decimal number is {1} , your string is {2}".format(int, flt, str))
