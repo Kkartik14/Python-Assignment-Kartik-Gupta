69cd283090bf620b8d40c1d802dd5c4b04be6f78
Kkartik14 <85060731+Kkartik14@users.noreply.github.com>
2023-03-17 14:51:19 +0530

    Delete page-11_problem_8.py

--- page-11_problem_8.py
+++
@@ -1,6 +0,0 @@
-# Convert all lower cases to upper case in a string.
-
-string = input("Enter a string: ")
-string_upper = string.upper()
-print("Original string:", string)
-print("String in uppercase:", string_upper)
